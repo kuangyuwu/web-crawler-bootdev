b6f7580ab0c335321b3e7c6bedf9df848bbfd469
kuangyuwu <kuangyuwu95@gmail.com>
2024-07-17 14:08:22 -0500

    main.js handle command line arguments

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello world!")
+import { argv } from "node:process";
+
+function main() {
+  if (argv.length < 3) {
+    console.log(`no url provided`);
+    console.log(`run the program by "npm run start BASE_URL"`);
+    return;
+  }
+  if (argv.length < 3) {
+    console.log(`too many arguments`);
+    console.log(`run the program by "npm run start BASE_URL"`);
+    return;
+  }
+  const url = argv[2];
+  console.log(`Start crawling ${url} ...`);
+}
+
+main();
